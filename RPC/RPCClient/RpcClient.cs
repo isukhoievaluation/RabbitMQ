830bada4bffd9746e55df9fad8c360639f2c4bc3
Ivan Sukhoi <ivan.hiqo-solutions.com>
2023-03-17 21:16:57 +0300

    add "RPC" solution

---
+++ RPC/RPCClient/RpcClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RPCClient
+{
+    public class RpcClient : IDisposable
+    {
+        private const string QUEUE_NAME = "rpc_queue";
+
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly string _replyQueueName;
+        private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> _callbackMapper = new ConcurrentDictionary<string, TaskCompletionSource<string>>();
+
+        public RpcClient()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _replyQueueName = _channel.QueueDeclare().QueueName;
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                if (!_callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out var tcs))
+                {
+                    return;
+                }
+
+                var body = ea.Body.ToArray();
+                var response = Encoding.UTF8.GetString(body);
+                tcs.TrySetResult(response);
+            };
+            _channel.BasicConsume(consumer: consumer, queue: _replyQueueName, autoAck: true);
+        }
+
+        public Task<string> CallAsync(string message, CancellationToken cancellationToken = default)
+        {
+            IBasicProperties props = _channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = _replyQueueName;
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            var tcs = new TaskCompletionSource<string>();
+            _callbackMapper.TryAdd(correlationId, tcs);
+
+            _channel.BasicPublish(exchange: string.Empty, routingKey: QUEUE_NAME, basicProperties: props, body: messageBytes);
+            cancellationToken.Register(() => _callbackMapper.TryRemove(correlationId, out _));
+
+            return tcs.Task;
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+    }
+}
